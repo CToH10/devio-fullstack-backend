fab2f613bb0a623134dd5e9c7d56563c241d606f
CToH10 <luis.ebn98@gmail.com>
2024-03-19 17:03:31 -0300

    feat: add error handling

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import cors from 'cors';
 import express, { Application, Request } from 'express';
+import { ErrorHandler } from './errors';
 
 export const app: Application = express();
 
 app.use(express.json());
 app.use(cors<Request>());
+
+// routes
+
+app.use(ErrorHandler);
