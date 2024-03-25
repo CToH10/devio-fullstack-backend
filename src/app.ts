df582532336a8d47a61e08ccb27e3cc2cf99ada3
CToH10 <luis.ebn98@gmail.com>
2024-03-24 23:38:52 -0300

    feat: add read routes and schemas

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,15 @@ import { additionalRoutes } from './routes/additionals.routes';
 import { orderRoutes } from './routes/orders.routes';
 import { productsRoutes } from './routes/products.routes';
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 export const app: Application = express();
 
 app.use(express.json());
 app.use(cors<Request>());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/products', productsRoutes);
 app.use('/orders', orderRoutes);
 app.use('/add', additionalRoutes);
